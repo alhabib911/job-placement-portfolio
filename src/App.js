a373c041680f235cd926e9740eef91b51555561d
alhabib911 <abdullahalhabib100@gmail.com>
2023-07-13 22:49:09 +0600

    Portfolio added in github

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import './App.css';
+import Header from './Components/Header/Header';
+
+function App() {
+  return (
+    <div className='bg-[#F3F2EF]'>
+      <Header />
+      <div className='px-2 pt-2'>
+        <h2>Activity</h2>
+      </div>
+      <div className='px-2 pt-2'>
+        <h2>Blogs</h2>
+      </div>
+      <div className='px-2 pt-2'>
+        <h2>Job Experience</h2>
+      </div>
+      <div className='px-2 pt-2'>
+        <h2>It Skills</h2>
+      </div>
+      <div className='px-2 pt-2'>
+        <h2>Soft Skills</h2>
+      </div>
+      <div className='px-2 pt-2'>
+        <h2>Web Development Projects</h2>
+      </div>
+      <div className='px-2 pt-2'>
+        <h2>Education</h2>
+      </div>
+      <div className='px-2 pt-2'>
+        <h2>Course</h2>
+      </div>
+      <div className='px-2 pt-2'>
+        <h2>Certificates</h2>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Header/Header.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import CoverPhoto from '../../image/cover-photo.jpeg'
+import ProfilePhoto from '../../image/profile-photo.jpeg'
+import BlogOne from '../../image/Blog/blog-one.png'
+
+const Header = () => {
+    return (
+        <div className='mx-20 flex gap-5 pt-5'>
+            <div className='w-3/4 bg-white shadow-md rounded-md '>
+                <div className='absolute flex'>
+                    <div className='w-8/12'>
+                        <img className='rounded-md' src={CoverPhoto} alt="" />
+                    </div>
+                </div>
+                <div className='relative pt-36 pl-6 '>
+                    <img className='w-[160px]' src={ProfilePhoto} alt="" />
+                </div>
+                <div className='pt-6 pl-6 flex justify-between gap-8'>
+                    <div className='w-8/12 '>
+                        <h1 className='text-2xl font-semibold'>Abdullah Al Habib</h1>
+                        <p className='pt-2'>Job Placement Intern at Programming Hero || Establishing connections worldwide and helping Recruiters to hire skilled Developers completely free of cost || MERN stack developer</p>
+                        <p className='text-sm text-gray-500 pt-3'>Talks about #planning, #teamwork, #decisionmaking, #hiringdevelopers, and #projectmanagement</p>
+                    </div>
+                    <div className='w-2/6'>
+                        <h1 className='text-base font-semibold'>Programming Hero</h1>
+                        <h1 className='text-base font-semibold'>Programming Hero</h1>
+                    </div>
+                </div>
+                <div className='pl-6 pt-3 flex items-center gap-5 pb-5'>
+                    <p className='text-base text-gray-500 pt-1'>Dhaka, Bangladesh</p>
+                    <button className='text-blue-600 font-semibold'>Contact Details</button>
+                </div>
+            </div>
+
+            <div className='w-1/4'>
+                <div className='bg-white rounded-md shadow-md pb-5'>
+                    <img className='rounded-md' src={BlogOne} alt="" />
+                    <div className='mx-2'>
+                        <h2 className='font-semibold text-lg pt-2'>Lorem ipsum dolor sit amet.</h2>
+                        <p className='pt-2 text-sm'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, pariatur quae. Consequatur.</p>
+                    </div>
+                    <div className='pt-3 mx-2'>
+                        <p className='text-sm'>Author: Abdullah Al Habib</p>
+                    </div>
+                </div>
+                <div className='bg-white rounded-md shadow-md pb-5 mt-2'>
+                    <div className='px-2 pt-2'>
+                        <h2>Meet With Me</h2>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
